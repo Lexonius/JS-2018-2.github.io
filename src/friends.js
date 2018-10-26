71adaa2f273f3ea793701b5d63e7780125c0030c
Lexonius <lexon33@mail.ru>
2018-10-26 12:51:08 +0300

    first commit

--- src/friends.js
+++ src/friends.js
@@ -2,125 +2,6 @@ import './styles/styles.css';
 // // import { auth } from './utils'
 import renderFn from './templates/template.hbs'
 
-//         // проврка: если друзья есть и в левом и в правом списке, то я их кладу в свои массивы, если их нет, то я беру friends.items и кладу в левый список, в правый пустой массив
-//         if(localStorage.getItem('store')){
-//             let friendsParse = JSON.parse(localStorage.getItem('store'))
-//             alert("nice")
-//             leftListArray = friendsParse.leftList;
-//             rightListArray = friendsParse.rightList;
-//             renderFriendsListLeft();
-//             renderFriendsListRight();
-//         } else {
-//             leftListArray = friends.items;
-//             rightListArray = [];
-//             renderFriendsListLeft();
-//             renderFriendsListRight();
-        
-        
-        
-//         // по нажатию на кнопку внутри контейнера, выполняется поиск элемента, внутри которого лежит кнопка, после чего элемент переносится
-//         // в правый контейнер и удаляется из левого
-//         friendsListLeft.addEventListener('click', (e) => {
-//             // смотрим, на какой элемент приходится клик
-//             let targetButton = e.target;
-//             // если клик пришелся не на изображение кнопки, то
-//             if(targetButton.tagName !== 'IMG'){
-//                 //завершаем работу функции
-//                 return
-//             } else {
-//                 //определяем id у кнопки
-//                 let id = targetButton.getAttribute('data-id')
-//                 //выявляем индекс элмента через id, на которую пришелся клик (обрати внимание на то, что id приходит числом, а нужен строкой)
-//                 const friendIndex = leftListArray.findIndex((friend => friend.id === Number(id)))
-//                 //пушим в правый массив элемент с индексом, который нам вернул findIndex
-//                 rightListArray.push(leftListArray[friendIndex])
-//                 //вырезаем элемент с полученным выше индексом
-//                 leftListArray.splice(friendIndex, 1);
-//                 //заново отрисовываем отфильтрованные массивы
-//                 renderFriendsListLeft();
-//                 renderFriendsListRight();
-//             }
-//         })
-//         // по нажатию на кнопку внутри контейнера, выполняется поиск элемента, внутри которого лежит кнопка, после чего элемент переносится
-//         // в левый контейнер и удаляется из правого (логику работы смотри выше)
-//         friendsListRight.addEventListener('click', (e) => {
-//             let targetButton = e.target;
-//             if(targetButton.tagName !== 'IMG'){
-//                 return
-//             } else {
-//                 let id = targetButton.getAttribute('data-id')
-//                 const friendIndex = rightListArray.findIndex ((friend => friend.id === Number(id)))
-//                 leftListArray.push(rightListArray[friendIndex])
-//                 rightListArray.splice(friendIndex, 1)
-//                 renderFriendsListLeft();
-//                 renderFriendsListRight();
-//             }
-//         })
-
-        
-//         //создаем пустой массив, в который кладем первоначальный массив. Получается массив, изменив который мы не затронем первоначальный массив.
-//         let filterFriendsLeftArr = [];
-//         filterFriendsLeftArr = leftListArray;
-//         let filterFriendsRightArr = [];
-//         filterFriendsRightArr = rightListArray;
-
-//         // по нажатию на клавишу в поле ввода вызывается функция по отрисовке левого списка
-//         filterInputLeft.addEventListener('keyup',() => {
-//             renderFriendsListLeft();
-//         })
-
-//         // по нажатию на клавишу в поле ввода вызывается функция по отрисовке правого списка
-//         filterInputRight.addEventListener('keyup',() => {
-//             renderFriendsListRight();
-//         })
-
-//         // функция по рендерингу левого списка друзей
-//         function renderFriendsListLeft(){
-//             // если в левое поле ввода есть значения
-//             if(filterInputLeft.value){
-//                 // то фильтруем массив с друзьями из левого списка (по имени, фамилии)
-//                 filterFriendsLeftArr = leftListArray.filter((item) => isMatching (item.first_name + " " + item.last_name, filterInputLeft.value))
-//             } else {
-//                 //если поле ввода пустое, то на отрисовку отдаем неизмененный первоначальный массив
-//                 filterFriendsLeftArr = leftListArray;
-//             }
-//             const leftListHtml = renderFn ({ my_friends: filterFriendsLeftArr, isLeft: true });
-//             friendsListLeft.innerHTML = leftListHtml;
-//         }
-
-//         //функция по рендерингу правого списка друзей (смотри выше)
-//         function renderFriendsListRight(){
-//             if(filterInputRight.value){
-//                 filterFriendsRightArr = rightListArray.filter((item) => isMatching (item.first_name + " " + item.last_name, filterInputRight.value))
-//             } else {
-//                 filterFriendsRightArr = rightListArray;
-//             }
-//             const rightListHtml = renderFn ({ my_friends: filterFriendsRightArr, isLeft: false });
-//             friendsListRight.innerHTML = rightListHtml;
-            
-//         }
-//         // функция по проверке подстроки в строке
-//         function isMatching(full, chunk) {
-//             if(full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1){
-//               return true;
-//             } else {
-//               return false;
-//             }
-//           }
-         
-
-//         let storage = localStorage;
-//             //сохранение данных в LocalStorage
-//             const buttonSave = document.querySelector('.footer_submit');
-//             buttonSave.addEventListener('click',() =>{
-//                 localStorage.setItem('store', JSON.stringify({
-//                     leftList: filterFriendsLeftArr,
-//                     rightList:filterFriendsRightArr
-//                 }))
-//                 alert('Data OK')
-//             })
-
-// проврка: если друзья есть и в левом и в правом списке, то я их кладу в свои массивы, если их нет, то я беру friends.items и кладу в левый список, в правый пустой массив
 let nameInput = document.querySelector('.input_name')
 let commentInput = document.querySelector('.input_comment')
 let addButton = document.querySelector('.button__add')
@@ -129,60 +10,60 @@ let commentsArr = [];
 let commentList = document.querySelector('.comments_list')
 
 
-        if(localStorage.getItem('store')){
-            let commentParse = JSON.parse(localStorage.getItem('store'))
-            // alert("nice")
-            commentsArr = commentParse.commList;
-            // console.log(commentsArr)
-            renderComments(commentsArr)
-        } else {
-            commentsArr = []
-            renderComments()
-        }
-
-
-
-
+if(localStorage.getItem('store')){
+    let commentParse = JSON.parse(localStorage.getItem('store'))
+    // alert("nice")
+    commentsArr = commentParse.commList;
+    // console.log(commentsArr)
+    renderComments(commentsArr)
+} else {
+    commentsArr = []
+    renderComments()
+}
 
 addButton.addEventListener('click',() => {
-    if( !nameInput.value && !commentInput.value){
-        console.log('14')
-        return
-    } else if(nameInput.value !== ' ' && commentInput.value !== ' '){
-        // nameInput.style.border = "3px solid #6BCBB6"
-        // commentInput.style.border = "3px solid #6BCBB6"
-        getComments()
-        // renderComments()
-    } else if(nameInput.value === ' ' && commentInput.value === ' '){
-        console.log('14')
-        nameInput.style.border = "3px solid #BE4F4F"
-        commentInput.style.border = "3px solid #BE4F4F"
-        // nameInput.style.border = "3px solid #6BCBB6"
+    console.log(nameInput.value)
+    console.log(commentInput.value)
+    let isInvalid = false;
+
+    if (!nameInput.value) {
+        nameInput.style.border = "3px solid #BE4F4F";
+        isInvalid = true;
     }
+
+    if (!commentInput.value) {
+        commentInput.style.border = "3px solid #BE4F4F";
+        isInvalid = true;
+    }
+
+    if (isInvalid) {
+        return;
+    }
+
+    getComments();
 })
+
 nameInput.addEventListener('keydown',() =>{
     nameInput.style.border = "3px solid #6BCBB6"
 })
+
 commentInput.addEventListener('keydown',() =>{
     commentInput.style.border = "3px solid #6BCBB6"
 })
+
 addButton.addEventListener('mouseover',() => {
-    console.log('5')
     addButton.style.background = '#53BBA8';
 })
 
 addButton.addEventListener('mouseout',() => {
-    console.log('6')
     addButton.style.background = '#6BCBB6';
 })
 
 buttonSave.addEventListener('mouseover',() => {
-    console.log('5')
     buttonSave.style.background = '#53BBA8';
 })
 
 buttonSave.addEventListener('mouseout',() => {
-    console.log('6')
     buttonSave.style.background = '#6BCBB6';
 })
 
@@ -203,7 +84,6 @@ function getComments(){
         },             
         id: generateId,
         comment: commentText,
-        remove: generateId
     }
     commentsArr.push(commentObj);
     renderComments()
@@ -212,29 +92,24 @@ function getComments(){
     
 
 function renderComments(){
-    nameInput.value = " ",
-    commentInput.value = " ";
+    nameInput.value = "",
+    commentInput.value = "";
     const comment = renderFn ({comments: commentsArr})
     commentList.innerHTML = comment
     
 }
-let comment = document.querySelector('.comment')
-// console.log(buttonRemove)
 
 commentList.addEventListener('click', (e) =>{
     let targetButton = e.target;
     if(targetButton.className !== 'remove__button-img'){
         return
     } else {
-        let id = targetButton.getAttribute('id')
+        let id = targetButton.getAttribute('data-id')
         const commentIndex = commentsArr.findIndex((a => a.id === Number(id)))
         commentsArr.splice(commentIndex, 1)
-        renderComments()
-
-        
+        renderComments();
     }            
 })
-
 
 buttonSave.addEventListener('click',() => {
     localStorage.setItem('store', JSON.stringify({
@@ -243,20 +118,15 @@ buttonSave.addEventListener('click',() => {
     // alert('Data OK')
 })
 
+window.dragStart = function(e){
+    e.dataTransfer.effectAllowed='move';
+    e.dataTransfer.setData("Text", e.target.getAttribute('id'));   
+    return true;
+}
 
    
 
-    
 
-            // //сохранение данных в LocalStorage
-            // const buttonSave = document.querySelector('.footer_submit');
-            // buttonSave.addEventListener('click',() =>{
-            //     localStorage.setItem('store', JSON.stringify({
-            //         leftList: filterFriendsLeftArr,
-            //         rightList:filterFriendsRightArr
-            //     }))
-            //     alert('Data OK')
-            // })
 
 
 
