97d5829cefc32a85a1ae389ae29737318a376976
Lexonius <lexon33@mail.ru>
2018-10-29 18:24:16 +0300

    fourth changes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
-// console.log('hello!')
-
-
-const mapModule = {
-    init: () => {
-        console.log('init!!')
-        ymaps.ready(init);    
-        function init(){ 
-            myMap = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 7
-            });   
-        }
-       
-    },
-
-    addMarker: () =>{
-        myMap.events.add('click', () =>{
-            console.log('66')
-            var coords = e.get('coords');
-            alert(coords.join(', '));
-
-        })
-    }
-
-    
-    
+const init = () =>{
+    const map = new ymaps.Map('map', {
+        center: [55.76, 37.64],
+        zoom: 7,
+        controls: []
+    })
+    map.events.add('click', e => {
+        console.log('click')
+    })
 }
-window.onload = mapModule.init();
-window.onload = mapModule.addMarker();
-
+ymaps.ready(init)
