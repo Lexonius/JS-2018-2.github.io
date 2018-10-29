7a10ddd53a8fe0c367fda31344e44911622dd23c
Lexonius <lexon33@mail.ru>
2018-10-29 17:47:00 +0300

    third changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('hello!')
+// console.log('hello!')
+
+
+const mapModule = {
+    init: () => {
+        console.log('init!!')
+        ymaps.ready(init);    
+        function init(){ 
+            myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 7
+            });   
+        }
+       
+    },
+
+    addMarker: () =>{
+        myMap.events.add('click', () =>{
+            console.log('66')
+            var coords = e.get('coords');
+            alert(coords.join(', '));
+
+        })
+    }
+
+    
+    
+}
+window.onload = mapModule.init();
+window.onload = mapModule.addMarker();
+
